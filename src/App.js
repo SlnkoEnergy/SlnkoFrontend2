9403be05e35340e9ac2e90c191aeae222b9d5895
slnko-energy <it@slnkoenergy.com>
2024-11-25 11:49:39 +0530

    git

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import React from 'react';
 import './App.css';
 
 
-// import Login from '../src/Components/login_page';
-import ForgotPassword from './Components/forgot_password';
+import Login from '../src/Components/login_page';
+// import ForgotPassword from './Components/forgot_password';
 
 function App() {
   return (
     <div>
-      {/* <Login /> */}
-      <ForgotPassword />
+      <Login />
+      {/* <ForgotPassword /> */}
     </div>
   );
 }
