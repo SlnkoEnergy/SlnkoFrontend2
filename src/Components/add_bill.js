d8a33dbbaad6d7d73593d10acba3deee9653fc24
KumarRounik <rounikpandey@gmail.com>
2025-01-07 17:20:24 +0530

    commited add bill joy

--- src/Components/add_bill.js
+++ src/Components/add_bill.js
@@ -119,7 +119,7 @@ const AddBillForm = () => {
         }}
       >
         <Box textAlign="center" mb={3}>
-        <img
+          <img
             src={Img10}
             alt="logo-icon"
             style={{ height: "50px", marginBottom: "10px" }}
@@ -128,7 +128,7 @@ const AddBillForm = () => {
             Add Bill
           </Typography>
         </Box>
-
+  
         {loading ? (
           <Typography textAlign="center">Loading...</Typography>
         ) : error ? (
@@ -140,18 +140,22 @@ const AddBillForm = () => {
             <Grid container spacing={2}>
               {/* Row 1 */}
               <Grid xs={12} md={4}>
+                <Typography level="body1" fontWeight="bold" mb={1}>
+                  Project ID
+                </Typography>
                 <Input
                   fullWidth
-                  placeholder="Project ID"
                   name="p_id"
                   value={formValues.p_id}
                   onChange={handleChange}
                 />
               </Grid>
               <Grid xs={12} md={4}>
+                <Typography level="body1" fontWeight="bold" mb={1}>
+                  PO Number
+                </Typography>
                 <Input
                   fullWidth
-                  placeholder="PO Number"
                   name="po_number"
                   value={formValues.po_number}
                   onChange={handleChange}
@@ -159,22 +163,26 @@ const AddBillForm = () => {
                 />
               </Grid>
               <Grid xs={12} md={4}>
+                <Typography level="body1" fontWeight="bold" mb={1}>
+                  Vendor
+                </Typography>
                 <Input
                   fullWidth
-                  placeholder="Vendor"
                   name="vendor"
                   value={formValues.vendor}
                   onChange={handleChange}
                   required
                 />
               </Grid>
-
+  
               {/* Row 2 */}
               <Grid xs={12} md={4}>
+                <Typography level="body1" fontWeight="bold" mb={1}>
+                  PO Date
+                </Typography>
                 <Input
                   fullWidth
                   type="date"
-                  placeholder="PO Date"
                   name="date"
                   value={formValues.date}
                   onChange={handleChange}
@@ -182,9 +190,11 @@ const AddBillForm = () => {
                 />
               </Grid>
               <Grid xs={12} md={4}>
+                <Typography level="body1" fontWeight="bold" mb={1}>
+                  Item Name
+                </Typography>
                 <Input
                   fullWidth
-                  placeholder="Item Name"
                   name="item"
                   value={formValues.item}
                   onChange={handleChange}
@@ -192,21 +202,25 @@ const AddBillForm = () => {
                 />
               </Grid>
               <Grid xs={12} md={4}>
+                <Typography level="body1" fontWeight="bold" mb={1}>
+                  PO Value (with GST)
+                </Typography>
                 <Input
                   fullWidth
-                  placeholder="PO Value (with GST)"
                   name="po_value"
                   value={formValues.po_value}
                   onChange={handleChange}
                   required
                 />
               </Grid>
-
+  
               {/* Row 3 */}
               <Grid xs={12} md={4}>
+                <Typography level="body1" fontWeight="bold" mb={1}>
+                  Bill Number
+                </Typography>
                 <Input
                   fullWidth
-                  placeholder="Bill Number"
                   name="bill_number"
                   value={formValues.bill_number}
                   onChange={handleChange}
@@ -214,10 +228,12 @@ const AddBillForm = () => {
                 />
               </Grid>
               <Grid xs={12} md={4}>
+                <Typography level="body1" fontWeight="bold" mb={1}>
+                  Bill Date
+                </Typography>
                 <Input
                   fullWidth
                   type="date"
-                  placeholder="Bill Date"
                   name="bill_date"
                   value={formValues.bill_date}
                   onChange={handleChange}
@@ -225,29 +241,35 @@ const AddBillForm = () => {
                 />
               </Grid>
               <Grid xs={12} md={4}>
+                <Typography level="body1" fontWeight="bold" mb={1}>
+                  Bill Value
+                </Typography>
                 <Input
                   fullWidth
-                  placeholder="Bill Value"
                   name="bill_value"
                   value={formValues.bill_value}
                   onChange={handleChange}
                   required
                 />
               </Grid>
-
+  
               {/* Row 4 */}
               <Grid xs={12}>
+                <Typography level="body1" fontWeight="bold" mb={1}>
+                  Type of Bill
+                </Typography>
                 <Autocomplete
                   options={billTypes}
                   getOptionLabel={(option) => option.label}
                   value={
-                    billTypes.find((type) => type.value === formValues.bill_type) || null
+                    billTypes.find((type) => type.value === formValues.bill_type) ||
+                    null
                   }
                   onChange={handleAutocompleteChange}
-                  placeholder="Type of Bill"
+                  placeholder="Select Type"
                 />
               </Grid>
-
+  
               {/* Submit Button */}
               <Grid xs={12} textAlign="center">
                 <Button type="submit" color="primary" sx={{ mx: 1 }}>
@@ -263,6 +285,8 @@ const AddBillForm = () => {
       </Box>
     </Box>
   );
+  
+  
 };
 
 export default AddBillForm;
